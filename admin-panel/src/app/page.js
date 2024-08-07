71ee4a71c595093ac55091f90c28b50952ae0877
khushigarg1 <khushigarg.64901@gmail.com>
2024-08-07 16:44:55 +0530

    chore: dashboard

--- admin-panel/src/app/page.js
+++ admin-panel/src/app/page.js
@@ -83,7 +83,7 @@ const Dashboard = () => {
             <ResponsiveContainer width="100%" height={300}>
               <PieChart>
                 <Pie
-                  data={pieData}
+                  data={pieData2}
                   cx="50%"
                   cy="50%"
                   labelLine={false}
