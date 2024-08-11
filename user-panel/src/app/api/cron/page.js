6694d62cab38007243565bab49aa7b08c2e3364e
khushigarg1 <khushigarg.64901@gmail.com>
2024-08-11 14:59:53 +0530

    chore: select label

--- user-panel/src/app/api/cron/page.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
-export async function GET() {
-  const response = await axios.get("https://ayru-project.onrender.com/api/availability/check-expired")
-  console.log(response);
-
-  return NextResponse.json({ ok: true });
-}
