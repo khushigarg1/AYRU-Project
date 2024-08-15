bff0fb180aae96aa1e1d192ed829ec5e83c2289a
khushigarg1 <khushigarg.64901@gmail.com>
2024-08-15 15:22:46 +0530

    chore: design of arrivals and might also like

--- user-panel/src/components/Inventory/ExtraColorItem.js
+++ user-panel/src/components/Inventory/ExtraColorItem.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, Card, CardContent, CardMedia, Typography, useTheme, useMediaQuery, Divider, Box } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Card, CardContent, CardMedia, Typography, useTheme, useMediaQuery, Box } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 import "./styles.css";
 
 export const InventoryAccordion = ({ relatedInventories }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: isMobile ? 3 : 4,
-    slidesToScroll: 2,
-    arrows: false
-  };
-
   return (
     <Accordion sx={{ marginTop: "0px", boxShadow: "none" }} defaultExpanded disableGutters>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         sx={{
-          marginTop: "0px", // Adjust margin top
-          marginBottom: "0px", // Adjust margin bottom
+          marginTop: "0px",
+          marginBottom: "0px",
           boxShadow: "none",
           minHeight: 0,
           paddingLeft: "1px"
@@ -34,50 +22,69 @@ export const InventoryAccordion = ({ relatedInventories }) => {
         <Typography sx={{ fontWeight: "bold" }}>You might also like</Typography>
       </AccordionSummary>
       <AccordionDetails sx={{ padding: "5px" }}>
-        <Slider {...settings}>
+        <Box
+          sx={{
+            display: 'flex',
+            overflowX: 'auto',
+            gap: 2,
+            paddingBottom: '10px',
+            '::-webkit-scrollbar': { display: 'none' } // Hide scrollbar for webkit browsers
+          }}
+        >
           {relatedInventories.map((inventory, index) => (
-            <Card key={inventory.id}
-              onClick={() => window.location.href = `/shop/${inventory.id}`} sx={{
-                height: "220px", padding: "0px 0px", boxShadow: "none", border: "1px solid black", marginRight: "15px", width: "90%", cursor: "pointer"
-              }}>
+            <Card
+              key={inventory.id}
+              onClick={() => window.location.href = `/shop/${inventory.id}`}
+              sx={{
+                height: "220px",
+                padding: "0px",
+                boxShadow: "none",
+                border: "1px solid gray",
+                // marginRight: "15px",
+                width: isMobile ? "35%" : "20%",
+                cursor: "pointer",
+                flexShrink: 0
+              }}
+            >
               <CardMedia
                 component="img"
                 image={inventory.Media && inventory.Media.length > 0 ? `https://ayrujaipur.s3.amazonaws.com/${inventory.Media[0].url}` : '/fallback_image_url'}
                 alt={inventory.productName}
                 sx={{
-                  objectFit: 'contain',
+                  objectFit: 'fit',
+                  // objectFit: 'contain',
                   height: "150px",
                   width: "100%"
                 }}
               />
-              {/* <Divider sx={{ borderStyle: "dotted", mt: 1 }} /> */}
               <CardContent sx={{ mt: 0, padding: "5px" }}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {inventory.colorVariation}
+                <Typography variant="body2" color={inventory?.colorVariation || 'black'} component="p"
+                  style={{ textTransform: "capitalize" }}
+                >
+                  {inventory?.colorVariation}
                 </Typography>
                 {inventory.discountedPrice ? (
-                  <Box sx={{ gap: 1 }}>
-                    <Typography variant="body2" sx={{ textDecoration: 'line-through' }}>
-                      ₹ {inventory?.sellingPrice?.toFixed(2)}
+                  <Box sx={{ display: "flex", direction: "row", gap: 1, flexDirection: "row" }}>
+                    <Typography variant="body2" sx={{ textDecoration: 'line-through', fontSize: "12px" }}>
+                      ₹ {inventory?.sellingPrice}
                     </Typography>
-                    <Typography variant="body2" >
+                    <Typography variant="body1" sx={{ fontSize: "12px" }}>
                       ₹ {inventory?.discountedPrice?.toFixed(2)}
                     </Typography>
                   </Box>
                 ) : (
-                  <Typography variant="body2">
+                  <Typography variant="body1" sx={{ fontSize: "12px" }}>
                     ₹ {inventory?.sellingPrice?.toFixed(2)}
                   </Typography>
                 )}
-                <Typography variant='body2' sx={{ color: inventory?.extraOptionOutOfStock ? 'red' : 'green' }}>
+                <Typography variant='body2' sx={{ color: inventory?.extraOptionOutOfStock ? 'red' : 'green', fontSize: "12px" }}>
                   {inventory.extraOptionOutOfStock ? 'Out of Stock' : 'In Stock'}
                 </Typography>
               </CardContent>
             </Card>
           ))}
-        </Slider>
+        </Box>
       </AccordionDetails>
     </Accordion>
   );
 };
-
--- user-panel/src/components/productSlider.js
+++ user-panel/src/components/productSlider.js
@@ -1,45 +1,18 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import Slider from 'react-slick';
-import { Box, Card, CardContent, CardMedia, Typography, IconButton, useMediaQuery, useTheme, Divider } from '@mui/material';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { FavoriteBorderOutlined, FavoriteOutlined, ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+import { Box, Card, CardContent, CardMedia, Typography, IconButton, useMediaQuery, useTheme } from '@mui/material';
+import { FavoriteBorderOutlined, FavoriteOutlined } from '@mui/icons-material';
 import api from '../../api';
-import "./slider.css";
 import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 import { useAuth } from '@/contexts/auth';
-
-// Custom Arrow Components
-const NextArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <IconButton
-      onClick={onClick}
-      style={{ position: 'absolute', top: '50%', right: '5px', zIndex: 2, backgroundColor: 'transparent', transform: 'translateY(-30%)' }}
-    >
-      <ArrowForwardIos />
-    </IconButton>
-  );
-};
-
-const PrevArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <IconButton
-      onClick={onClick}
-      style={{ position: 'absolute', top: '50%', left: '5px', zIndex: 2, backgroundColor: 'transparent', transform: 'translateY(-30%)' }}
-    >
-      <ArrowBackIos />
-    </IconButton>
-  );
-};
+import "./slider.css";
 
 export const ProductSlider = ({ products }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const itemsPerPage = isMobile ? 2 : 5;
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'));
+  const itemsPerPage = isMobile ? 1 : 2;
   const router = useRouter();
   const slicedProducts = products.slice(0, 10);
   const { openAuthModal, user, wishlistCount, setWishlistCount } = useAuth();
@@ -110,124 +83,94 @@ export const ProductSlider = ({ products }) => {
     }
   };
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: itemsPerPage,
-    slidesToScroll: itemsPerPage,
-    arrows: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-    appendDots: dots => (
-      <Box component="ul" sx={{ margin: 0, padding: 0, display: 'flex', justifyContent: 'center', listStyle: 'none', bottom: "10px" }}>
-        {dots}
-      </Box>
-    ),
-    customPaging: i => (
-      <Box
-        component="div"
-        sx={{
-          width: '10px',
-          height: '10px',
-          backgroundColor: '#000',
-          borderRadius: '50%',
-          display: 'inline-block',
-          margin: '0 5px',
-        }}
-        className="slick-dot"
-      ></Box>
-    ),
-  };
-
   return (
-    <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: theme.palette.text.fontFamily, mt: 2 }}>
-      <Box style={{ width: "100%", display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: "2%" }}>
-        <Typography variant={isMobile ? 'h4' : 'h3'} sx={{ mb: 2, color: theme.palette.text.text }}>
-          New Arrivals
-        </Typography>
-      </Box>
-      <Box sx={{ width: '100%', margin: 'auto', position: 'relative', paddingBottom: '40px' }}>
-        <Slider {...settings}>
-          {slicedProducts.map((product, index) => (
-            <Box key={product.id} p={1} sx={{ backgroundColor: "transparent", position: 'relative' }}>
-              <Card sx={{ backgroundColor: "transparent", boxShadow: "none", position: 'relative', cursor: "pointer" }}
-                onClick={() => router.push(`/shop/${product?.id}`)}
-              >
-                <IconButton
-                  sx={{ position: 'absolute', top: 5, right: 10, zIndex: 1, backgroundColor: 'rgba(255, 255, 255, 0.8)' }}
-                  onClick={(event) => handleToggleWishlist(event, product)}
-                >
-                  {wishlistItems[product.id] ? <FavoriteOutlined style={{ color: 'red' }} /> : <FavoriteBorderOutlined />}
-                </IconButton>
-                <CardMedia
-                  component="img"
-                  height={isMobile ? 200 : 350}
-                  image={product.Media && product.Media.length > 0 ? `https://ayrujaipur.s3.amazonaws.com/${product.Media[0].url}` : '/fallback_image_url'}
-                  alt={product.productName}
-                  sx={{
-                    objectFit: 'contain',
-                    height: isMobile ? "200px" : "350px",
-                  }}
-                />
-                <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "left", height: "100px", maxHeight: "200px" }}>
-                  <Typography gutterBottom variant="body1" component="div" sx={{ fontSize: "14px", lineHeight: "1.1" }}>
-                    {product.productName}
+    <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
+      <Typography variant={isMobile ? 'h4' : 'h3'} sx={{ mb: 2, color: theme.palette.text.text }}>
+        New Arrivals
+      </Typography>
+
+      {/* Scrollable container */}
+      <Box
+        sx={{
+          display: 'flex',
+          overflowX: 'scroll',
+          gap: 2,
+          padding: '10px',
+          width: '100%',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          }
+          // "&::-webkit-scrollbar": {
+          //   height: '4px',
+          // },
+          // "&::-webkit-scrollbar-thumb": {
+          //   backgroundColor: theme.palette.primary.main,
+          //   borderRadius: '10px',
+          // },
+          // "&::-webkit-scrollbar-track": {
+          //   backgroundColor: theme.palette.background.default,
+          //   borderRadius: '10px',
+          // },
+        }}
+      >
+        {slicedProducts.map((product) => (
+          <Card
+            key={product.id}
+            sx={{
+              // minWidth: isMobile ? '30%' : '40%',
+              width: isMobile ? "42%" : isTablet ? "30%" : "20%",
+              flexShrink: 0,
+              backgroundColor: "transparent",
+              boxShadow: "none",
+              position: 'relative',
+              cursor: "pointer",
+            }}
+            onClick={() => router.push(`/shop/${product?.id}`)}
+          >
+            <IconButton
+              sx={{
+                position: 'absolute',
+                top: 2,
+                right: 2,
+                zIndex: 1,
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+              }}
+              onClick={(event) => handleToggleWishlist(event, product)}
+            >
+              {wishlistItems[product.id] ? <FavoriteOutlined style={{ color: 'red' }} /> : <FavoriteBorderOutlined />}
+            </IconButton>
+            <CardMedia
+              component="img"
+              height={isMobile ? 200 : 350}
+              image={product.Media && product.Media.length > 0 ? `https://ayrujaipur.s3.amazonaws.com/${product.Media[0].url}` : '/fallback_image_url'}
+              alt={product.productName}
+              sx={{
+                objectFit: 'fit',
+                height: isMobile ? 200 : 350,
+              }}
+            />
+            <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start", height: "100px", maxHeight: "200px", padding: "10px 0px" }}>
+              <Typography gutterBottom variant="body1" component="div" sx={{ fontSize: "14px", lineHeight: "1.1" }}>
+                {product.productName}
+              </Typography>
+              {product.discountedPrice ? (
+                <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 0.5 }}>
+                  <Typography variant="body2" sx={{ textDecoration: 'line-through', fontSize: "14px" }}>
+                    ₹{product.sellingPrice?.toFixed(2)}
                   </Typography>
-                  {/* <Typography variant="body2" gutterBottom>
-                    <strong>Category:</strong> {product?.Category?.categoryName}
-                  </Typography> */}
-                  {product.discountedPrice ? (
-                    <Box sx={{ display: 'flex', alignItems: 'left', gap: 0.5 }}>
-                      <Typography variant="body2" sx={{ textDecoration: 'line-through', fontSize: "14px" }}>
-                        ₹{product.sellingPrice?.toFixed(2)}
-                      </Typography>
-                      <Typography variant="body1" sx={{ marginRight: "3px", fontSize: "14px", fontWeight: "bold" }} >
-                        ₹{product.discountedPrice?.toFixed(2)}
-                      </Typography>
-                    </Box>
-                  ) : (
-                    <Typography variant="body1" sx={{ marginRight: "3px", fontSize: "14px", fontWeight: "bold" }} >
-                      ₹{product.sellingPrice?.toFixed(2)}
-                    </Typography>
-                  )}
-                </CardContent>
-              </Card>
-              {index < slicedProducts.length - 1 && <Divider orientation="vertical" sx={{ position: 'absolute', right: 0, top: 0, bottom: 0 }} />}
-            </Box>
-          ))}
-        </Slider>
-      </Box >
-    </Box >
+                  <Typography variant="body1" sx={{ fontSize: "14px", fontWeight: "bold" }}>
+                    ₹{product.discountedPrice?.toFixed(2)}
+                  </Typography>
+                </Box>
+              ) : (
+                <Typography variant="body1" sx={{ fontSize: "14px", fontWeight: "bold" }}>
+                  ₹{product.sellingPrice?.toFixed(2)}
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+    </Box>
   );
 };
